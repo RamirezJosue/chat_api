431034d3ba334d0354f2d0f3ac4e7bbe3c8c93a5
RamirezJosue <merling.ramirez@upeu.edu.pe>
2023-02-13 09:03:39 -0500

    first commit

---
+++ app/Notifications/MessageSent.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+use NotificationChannels\OneSignal\OneSignalChannel;
+use NotificationChannels\OneSignal\OneSignalMessage;
+
+
+class MessageSent extends Notification
+{
+    use Queueable;
+
+    public function __construct(private array $data)
+    {
+        //
+    }
+
+    public function via()
+    {
+        return [OneSignalChannel::class];
+    }
+
+    public function toOneSignal(){
+        $messageData = $this->data['messageData'];
+
+        return OneSignalMessage::create()
+                ->setSubject($messageData['senderName']. " sent you a message.")
+                ->setBody($messageData['message'])
+                ->setData('data',$messageData);
+    }
+
+
+}
